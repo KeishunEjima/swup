cc89721c5d87cb3cb9e5d0a16d170bde7b418882
ejima8686 <ejima@nevers.jp>
2025-04-21 17:21:57 +0900

    swupを使ったページ遷移アニメーション

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://keishunejima.github.io/",
-    base: "/swup/"
+  site: "https://keishunejima.github.io/",
+  base: "/swup/",
+  integrations: [tailwindcss()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
